2575e83c6fabb0e21718d74a3eed601c4a69908c
İbrahim Ethem Sancar <ibrahimethemsancar@gmail.com>
2022-01-11 16:15:18 +0300

    task-4

---
+++ 4.odev/index.js
@@ -0,0 +1,33 @@
+const fs=require('fs');
+
+//Dosya oluşturma
+
+// fs.writeFile('employees.json', '{"name": "Employee 1 Name", "salary": 2000}',"utf-8",(err)=>{
+//     if(err) console.log(err);
+//     console.log('dosya oluşturuldu.')
+// })
+
+//Dosya okuma
+
+// fs.readFile('employees.json','utf-8',(err,data)=>{
+//     if(err) console.log(err);
+//     console.log(data)
+//     console.log('Dosya okundu')
+// })
+
+
+//Veri Güncelleme
+
+// fs.writeFile('employees.json', '{"name": "Employee 1 Name", "salary": 12312}',"utf-8",(err)=>{
+//     if(err) console.log(err);
+//     console.log('veri güncellendi.')
+// })
+
+
+//Dosya silme
+
+// fs.unlink('employees.json',(err)=>{
+//     if(err) console.log(err);
+//     console.log('dosya silindi')
+// })
+
